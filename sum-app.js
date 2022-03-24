0fb6a8d5ecb28bead02adca119d7ab7f14e887c8
MJung84 <marko.jungschlaeger@gmail.com>
2022-03-24 09:46:09 +0100

    final submit

--- sum-app.js
+++ sum-app.js
@@ -1,3 +1,23 @@
 class SumApp {
-  // Add you solution here
+  constructor() {
+    this.numbers = [];
+  }
+
+  addNumber(n) {
+    this.numbers.push(n);
+  }
+
+  getSum() {
+    let result = 0;
+
+    for (let i = 0; i < this.numbers.length; i++) {
+      result += this.numbers[i];
+    }
+    //console.log(result);
+    return result;
+  }
+  reset() {
+    this.numbers = [];
+    //console.log(this.numbers);
+  }
 }
